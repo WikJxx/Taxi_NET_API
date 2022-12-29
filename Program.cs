effa6c2ff89f82e2104a46486f25b0e695da72c6
unknown <wikj.xxx@gmail.com>
2022-12-29 17:53:40 +0100

    IDK

--- Program.cs
+++ Program.cs
@@ -7,8 +7,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddDbContext<ElectricTaxiContext>(opt =>opt.UseInMemoryDatabase("ElectricTaxiList"));
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddDbContext<ElectricTaxiContext>(opt =>opt.UseInMemoryDatabase("ElectricTaxiList"));
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
